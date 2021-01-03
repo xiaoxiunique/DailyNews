c96487413d2e6ba6e6e82bacaa955eec1ff05157
xiejiuwei <xiejiuwei@aliyun.com>
2021-01-03 17:18:45 +0800

    refactor file

---
+++ data/csdn 推荐.md
@@ -0,0 +1,12 @@
+## csdn 推荐 
+
+- [要不是真的喜欢学技术，谁会来爬小姐姐啊。](https://blog.csdn.net/lovely__RR/article/details/107956207)
+- [&ldquo;滴滴打摩托&rdquo;是什么鬼？东南亚的这家公司给做出来了！](https://blog.csdn.net/bjweimengshu/article/details/109712575)
+- [YOLO系列之yolo v1](https://blog.csdn.net/leviopku/article/details/82588059)
+- [你不是学生，你是开发者](https://blog.csdn.net/weixin_44950174/article/details/106203163)
+- [html2canvas 与 jspdf 相结合生成 pdf 内容被截断的终极解决方案](https://blog.csdn.net/qq_36772866/article/details/109912907)
+- [Py之cv2：cv2库(OpenCV，opencv-python)的简介、安装、使用方法(常见函数、方法等)最强详细攻略](https://blog.csdn.net/qq_41185868/article/details/79675875)
+- [讲道理，只要你是一个爱折腾的程序员，毕业找工作真的不需要再花钱培训！](https://blog.csdn.net/Yao__Shun__Yu/article/details/105871278)
+- [python 考试题（带答案）](https://blog.csdn.net/yipianfeng_ye/article/details/89416439)
+- [2020年最新Android开发工程师成长路线(架构师,职业规划,NDK,Flutter,Framework,UI,性能优化)](https://blog.csdn.net/wq6ylg08/article/details/108915417)
+- [详解PHP的内存管理机制与垃圾回收机制](https://blog.csdn.net/liuxingjiaoyuC/article/details/109453202)
---
+++ index.js
@@ -0,0 +1,104 @@
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+const nodemailer = require('nodemailer');
+
+const [user, pass, tomail] = process.argv.slice(2);
+console.log('user, pass, tomail: ', user, pass, tomail);
+
+/**
+ * 根据请求配置 读取数据
+ */
+async function handleReqConf({ method, url, header, data = {}, contentField }) {
+  const axios = require('axios');
+  const config = {
+    method,
+    url,
+    headers: header,
+    data,
+  };
+
+  const result = await axios(config);
+  return _.get(result, contentField, []);
+}
+
+function readReqFlow() {
+  const filePath = path.resolve(__dirname, './src/reqConf.json');
+  const reqJsonSTR = fs.readFileSync(filePath, {
+    encoding: 'utf-8',
+  });
+  return JSON.parse(reqJsonSTR).requestConfList;
+}
+
+function writeData2MD(itemList, { parse, title }) {
+  const STR = itemList.reduce((acc, item) => {
+    const title = _.get(item, parse.title);
+    const jumpURL = (parse.baseURL || '') + _.get(item, parse.jumpURL);
+
+    return acc + `\n- [${title}](${jumpURL})`;
+  }, `## ${title} \n`);
+
+  fs.writeFileSync(path.resolve(__dirname, './data/' + title + '.md'), STR);
+}
+
+async function transferMD2Html() {
+  const fileList = fs.readdirSync(path.resolve(__dirname, './data'));
+
+  let htmlSTR = '';
+  for (const file of fileList) {
+    const mdSTR = fs.readFileSync(path.resolve(__dirname, './data/' + file), {
+      encoding: 'utf8',
+    });
+
+    const md = require('markdown').markdown;
+    const mdHTML = md.toHTML(mdSTR);
+    htmlSTR += mdHTML + '\n';
+  }
+
+  await sendMail(tomail, htmlSTR);
+  fs.writeFileSync(path.resolve(__dirname, './schedule-news.html'), htmlSTR);
+}
+
+async function sendMail(tomail = 'xiaoxiunique@qq.com', HTML) {
+  // Create a SMTP transporter object
+  let transporter = nodemailer.createTransport({
+    secure: true,
+    host: 'smtp.163.com',
+    auth: {
+      user: user,
+      //这里是授权密码而不是邮件密码
+      pass: pass,
+    },
+  });
+  // Message object
+  let message = {
+    from: user,
+
+    // Comma separated list of recipients
+    to: tomail,
+    // Subject of the message
+    subject: '每日新闻',
+    text: 'test',
+
+    // HTML body
+    html: HTML,
+  };
+
+  let info = await transporter.sendMail(message);
+  console.log('Message sent successfully as %s', info.messageId);
+}
+
+(async function () {
+  const reqFlow = readReqFlow();
+
+  for (const reqConf of [...reqFlow]) {
+    try {
+      const result = await handleReqConf(reqConf);
+      writeData2MD(result, reqConf);
+    } catch (e) {
+      console.error('读取失败', e.message);
+    }
+  }
+
+  await transferMD2Html();
+})();
